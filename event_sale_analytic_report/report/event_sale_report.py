5985ca6e2994fe891187c402adad2aa60e63e9fd
Murtuza Saleh <msaleh@opensourceintegrators.com>
2025-11-08 10:06:03 +0530

    [ENH] Added Event Duration (Hours) field and logic.

--- event_sale_analytic_report/report/event_sale_report.py
+++ event_sale_analytic_report/report/event_sale_report.py
@@ -14,14 +14,18 @@ class EventSaleReport(models.Model):
     )
     analytic_amount = fields.Float("Analytic amount", readonly=True)
 
+    # ==== Event duration (in hours) ====
+    event_duration = fields.Float("Event Duration (Hours)", readonly=True)
+
     def _select_clause(self, *select):
         return super()._select_clause(
-            """ 
+            """
             event_event.stage_id AS stage_id,
             template.categ_id AS product_category_id,
             aal.account_id AS account_id,
-            (aal.amount / NULLIF(sol_reg_count.count, 1)) AS analytic_amount
-        """,
+            (aal.amount / NULLIF(sol_reg_count.count, 0)) AS analytic_amount,
+            (ROUND(EXTRACT(EPOCH FROM (event_event.date_end - event_event.date_begin)) / NULLIF(sol_reg_count.count * 3600.0, 0), 2)) AS event_duration
+            """,
             *select
         )
 
@@ -39,6 +43,6 @@ class EventSaleReport(models.Model):
                 FROM event_registration erc
                 WHERE erc.sale_order_line_id = sale_order_line.id
             ) sol_reg_count ON TRUE
-        """,
+            """,
             *join_
         )
